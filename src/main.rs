c8334655d134ec88415a12a23d479323c0687c6b
meguminloli <meguminloli@protonmail.com>
2021-12-01 23:48:23 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::{fs::File, str::FromStr, io::{Read, Write}};
+
+use cookie_store::CookieStore;
+use serde::Deserialize;
+use time::OffsetDateTime;
+use ureq::Cookie;
+
+const CONFIG_FILE_NAME: &str = ".aocdown";
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    session_cookie: String,
+    year: Option<i32>,
+    day: Option<u8>,
+}
+
+fn main() {
+    let config_file = File::open(CONFIG_FILE_NAME).expect("Missing config file");
+    let config: Config = serde_json::from_reader(config_file).unwrap();
+    let time = OffsetDateTime::now_utc();
+    let year = time.year();
+    let day = time.day();
+    let url = format!(
+        "https://adventofcode.com/{}/day/{}/input",
+        if let Some(year) = config.year {
+            year
+        } else {
+            year
+        },
+        if let Some(day) = config.day {
+            day
+        } else {
+            day
+        }
+    );
+    let cookie = Cookie::new("session", config.session_cookie);
+    let mut my_store = CookieStore::load_json(&*Vec::new()).unwrap();
+    let url = url::Url::from_str(&url).unwrap();
+    let cookie = cookie_store::Cookie::try_from_raw_cookie(&cookie, &url).unwrap();
+    my_store.insert(cookie, &url).unwrap();
+    let agent = ureq::builder()
+        .cookie_store(my_store)
+        .build();
+    
+    let resp = agent.get(url.as_str()).call().unwrap();
+    let path = format!("{}/{}", year, day);
+    std::fs::create_dir_all(&path).ok();
+    let mut f = File::create(format!("{}/input.txt", &path)).unwrap();
+    let mut v = Vec::new();
+    resp.into_reader().read_to_end(&mut v).unwrap();
+    f.write(&v).unwrap();
+}
