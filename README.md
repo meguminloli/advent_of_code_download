74cd3754e3042eb83cd15cc36b8d6eb06c2828cb
meguminloli <meguminloli@protonmail.com>
2021-12-02 16:22:36 +0200

    fixed bug

---
+++ README.md
@@ -0,0 +1,7 @@
+# AOC downloader
+
+Downloads and creates a cargo repo on the run.
+
+It's for personal use, since it's a piece of shit, that I use to download the inputs on my phone.
+
+:trollface:
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{fs::File, str::FromStr, io::{Read, Write}, process::Command};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    process::Command,
+    str::FromStr,
+};
 
 use cookie_store::CookieStore;
 use serde::Deserialize;
@@ -12,6 +17,7 @@ struct Config {
     session_cookie: String,
     year: Option<i32>,
     day: Option<u8>,
+    init_cargo: Option<bool>,
 }
 
 const TEMPLATE: &str = r##"use std::fs;
@@ -21,35 +27,34 @@ fn main() {
 }
 "##;
 
-
 fn main() {
+    // Parse config file
     let config_file = File::open(CONFIG_FILE_NAME).expect("Missing config file");
     let config: Config = serde_json::from_reader(config_file).unwrap();
+
+    // Extract year and day
     let time = OffsetDateTime::now_utc();
-    let year = time.year();
-    let day = time.day();
-    let url = format!(
-        "https://adventofcode.com/{}/day/{}/input",
-        if let Some(year) = config.year {
-            year
-        } else {
-            year
-        },
-        if let Some(day) = config.day {
-            day
-        } else {
-            day
-        }
-    );
+    let year = if let Some(year) = config.year {
+        year
+    } else {
+        time.year()
+    };
+    let day = if let Some(day) = config.day {
+        day
+    } else {
+        time.day()
+    };
+
+    // Add the session cookie to agent
+    let url = format!("https://adventofcode.com/{}/day/{}/input", year, day,);
     let cookie = Cookie::new("session", config.session_cookie);
     let mut my_store = CookieStore::load_json(&*Vec::new()).unwrap();
     let url = url::Url::from_str(&url).unwrap();
     let cookie = cookie_store::Cookie::try_from_raw_cookie(&cookie, &url).unwrap();
     my_store.insert(cookie, &url).unwrap();
-    let agent = ureq::builder()
-        .cookie_store(my_store)
-        .build();
-    
+    let agent = ureq::builder().cookie_store(my_store).build();
+
+    // Write the response to input.txt
     let resp = agent.get(url.as_str()).call().unwrap();
     let path = format!("{}/{}", year, day);
     std::fs::create_dir_all(&path).ok();
@@ -58,11 +63,18 @@ fn main() {
     resp.into_reader().read_to_end(&mut v).unwrap();
     f.write(&v).unwrap();
 
-    let output = Command::new("cargo")
-        .args(["init", &path, "--name", &format!("day_{}", day)])
-        .output()
-        .unwrap();
-    let main_path = format!("{}/src/main.rs", path);
-    let mut f = File::create(&main_path).unwrap();
-    write!(f, "{}", TEMPLATE).unwrap();
+    // Initialize a cargo + git repo in the specific folder
+    if let Some(b) = config.init_cargo {
+        if b {
+            let _output = Command::new("cargo")
+                .args(["init", &path, "--name", &format!("day_{}", day)])
+                .output()
+                .unwrap();
+
+            // Write my own template for a quick start
+            let main_path = format!("{}/src/main.rs", path);
+            let mut f = File::create(&main_path).unwrap();
+            write!(f, "{}", TEMPLATE).unwrap();
+        }
+    }
 }
